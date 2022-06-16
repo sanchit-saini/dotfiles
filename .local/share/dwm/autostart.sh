453fc33d0400c8b7df40ea7d3ca2ba8b42d9d0e1
Sanchit Saini <sanchitsaini14@gmail.com>
2022-06-17 03:17:38 +0530

    Add dwm config and wallpaper

---
+++ .local/share/dwm/autostart.sh
@@ -0,0 +1,7 @@
+unclutter &
+picom &
+dunst &
+sxhkd &
+dwmblocks &
+xwallpaper --stretch ~/.local/share/dwm/wallpaper.jpg
+xinput set-prop "AUI1661:00 044E:120A Touchpad" "libinput Tapping Enabled" 1
